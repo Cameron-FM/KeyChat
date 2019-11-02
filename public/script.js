f6f88ed5ae4fdea11397bc623d9c0b8899bde855
Cameron Farrier <48474999+Cameron-FM@users.noreply.github.com>
2019-11-02 19:55:11 +0000

    Added Real-time Room Search
    - Added the ability to search and then join specifc rooms.
    
    - Rooms that match the search will appear under the search bar.
    
    - Input autocomplete was removed.

--- public/script.js
+++ public/script.js
@@ -1,63 +1,80 @@
 //Where the server is hosting the socket.js application
-const socket = io('http://localhost:3000')
+const socket = io("http://localhost:3000");
 
 //Get data from HTML elements
-const messageContainer = document.getElementById('message-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
-const searchForm = document.getElementById('search-bar')
-const searchBar = document.getElementById('search-input')
+const messageContainer = document.getElementById("message-container");
+const messageForm = document.getElementById("send-container");
+const messageInput = document.getElementById("message-input");
+const searchForm = document.getElementById("search-bar");
+const searchBar = document.getElementById("search-input");
+const roomsContainer = document.getElementById("room-container");
+
+let searchedForRooms = [];
 
 //Sends the users search to the server everytime they lift a key
-if (searchForm != null){
-  searchBar.addEventListener('keyup', e => {
-    let searchText = document.getElementById('search-input').value.toLowerCase()
-    socket.emit('room-search', socket.id, searchText)
-  })
+if (searchForm != null) {
+  searchBar.addEventListener("keyup", e => {
+    let searchText = document.getElementById("search-input").value.toLowerCase();
+    socket.emit("room-search", socket.id, searchText);
+  });
+
+  //searchedForRooms.forEach(room => {
+  //console.log(searchText);
+  //if (room.slice(0, searchText.length).includes(searchText)) {
+  //roomsContainer.removeChild(room + searchedForRooms.indexOf(room))
+  //}
+  //});
 }
 
- //Only runs when user joins a server
-if (messageForm != null){
+//Only runs when user joins a server
+if (messageForm != null) {
   //Asks user to enter their nickname when joining the server
-  const username = prompt('Enter Your Nickname:')
-  appendMessage('You Joined The Server')
-  socket.emit('new-user', roomName, username)
+  const username = prompt("Enter Your Nickname:");
+  appendMessage("You Joined The Server");
+  socket.emit("new-user", roomName, username);
 
   //Listen for submit button press
-  messageForm.addEventListener('submit', e => {
-    e.preventDefault() //Stop page from auto reloading when form is submitted
-    const message = messageInput.value
-    socket.emit('send-chat-message', roomName, message) //Send chat message to server
-    appendMessage(`${username}: ${message}`)
-    messageInput.value = '' //Clear the input field
-  })
+  messageForm.addEventListener("submit", e => {
+    e.preventDefault(); //Stop page from auto reloading when form is submitted
+    const message = messageInput.value;
+    socket.emit("send-chat-message", roomName, message); //Send chat message to server
+    appendMessage(`${username}: ${message}`);
+    messageInput.value = ""; //Clear the input field
+  });
 }
 
 //Recvices the items that match the users search
 //Doesn't work
-socket.on('search-items', items => {
-  console.log(items);
-})
+socket.on("search-items", room => {
+  if (!searchedForRooms.includes(room)) {
+    searchedForRooms.push(room);
+    let newEl = document.createElement('a');
+    newEl.setAttribute("id", room + searchedForRooms.indexOf(room));
+    newEl.setAttribute("class", "room");
+    newEl.setAttribute('href', "/" + room);
+    newEl.innerText = room;
+    roomsContainer.appendChild(newEl);
+  }
+});
 
 //Whenever the client recives a message run appendMessage()
-socket.on('recive-chat-message', data => {
-  appendMessage(`${data.name}: ${data.message}`)
-})
+socket.on("recive-chat-message", data => {
+  appendMessage(`${data.name}: ${data.message}`);
+});
 
 //Whenever a new user connects run appendMessage()
-socket.on('user-connected', username => {
-  appendMessage(`${username}: Connected To The Server`)
-})
+socket.on("user-connected", username => {
+  appendMessage(`${username}: Connected To The Server`);
+});
 
 //Whenever a new user connects run appendMessage()
-socket.on('user-disconnected', username => {
-  appendMessage(`${username}: Disconnected From The Server`)
-})
+socket.on("user-disconnected", username => {
+  appendMessage(`${username}: Disconnected From The Server`);
+});
 
 //Append a message element to the page containing the message data
-function appendMessage(message){
-  messageElement = document.createElement('div')
-  messageElement.innerText = message
-  messageContainer.append(messageElement)
-
+function appendMessage(message) {
+  messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageContainer.append(messageElement);
 }
--- server.js
+++ server.js
@@ -1,81 +1,85 @@
 //Create express app and server
-const express = require('express')
-const app = express()
-const server = require('http').Server(app) //Create http server
-const io = require('socket.io')(server) //Server will run on the socket.io connection (Port 3000)
+const express = require("express");
+const app = express();
+const server = require("http").Server(app); //Create http server
+const io = require("socket.io")(server); //Server will run on the socket.io connection (Port 3000)
 
 //Set up express server
-app.set('views', './views') //Set where are view will come from
-app.set('view engine', 'ejs') //Tell app views to use ejs
-app.use(express.static('public')) //State the public folder where client side JS will be held
-app.use(express.urlencoded({extended: true})) //Allows app to use URL encoded parameters inside of a body for a form
+app.set("views", "./views"); //Set where are view will come from
+app.set("view engine", "ejs"); //Tell app views to use ejs
+app.use(express.static("public")); //State the public folder where client side JS will be held
+app.use(express.urlencoded({ extended: true })); //Allows app to use URL encoded parameters inside of a body for a form
 
 //Create object for chat rooms
-const rooms = {}
+const rooms = {};
 
 //Create routes for the server
-app.get('/', (req, res) => {
-  res.render('index', {rooms: rooms})
-})
+app.get("/", (req, res) => {
+  res.render("index", { rooms: rooms });
+});
 
-app.post('/room', (req, res) => {
-  if (rooms[req.body.room] != null){ //If name of room already exists
-    return res.redirect('/') //Redirect to room select page
+app.post("/room", (req, res) => {
+  if (rooms[req.body.room] != null) {
+    //If name of room already exists
+    return res.redirect("/"); //Redirect to room select page
   }
-  rooms[req.body.room] = {users: {}}
-  res.redirect(req.body.room) //Redirect to choosen room
-})
+  rooms[req.body.room] = { users: {} };
+  res.redirect(req.body.room); //Redirect to choosen room
+});
 
-app.get('/:room', (req, res) => {
-  if (rooms[req.params.room] == null){
-    res.redirect('/')
+app.get("/:room", (req, res) => {
+  if (rooms[req.params.room] == null) {
+    return res.redirect("/");
   }
-  res.render('room', {roomName: req.params.room})
-})
+  res.render("room", { roomName: req.params.room });
+});
 
 //Server will listen on port 3000
-server.listen(3000)
+server.listen(3000);
 
 //Get the rooms a specific user is in
-function getUserRooms(socket){
+function getUserRooms(socket) {
   return Object.entries(rooms).reduce((names, [name, room]) => {
-    if (room.users[socket.id] != null) names.push(name) //If there is a user with this socket ID, push in the name of the room
-    return names //Returns to use in next itteration of the reduce method
-  }, [])
+    if (room.users[socket.id] != null) names.push(name); //If there is a user with this socket ID, push in the name of the room
+    return names; //Returns to use in next itteration of the reduce method
+  }, []);
 }
 
 //Log whenever a new user connects
-io.on('connection', socket => {
+io.on("connection", socket => {
   //Broadcast that a new user had joined to al other users in that specific room
-  socket.on('new-user', (room, username) => {
-    socket.join(room)
-    rooms[room].users[socket.id] = username //Assign the users name to their socket ID and store in the object with the room name their in
-    socket.to(room).broadcast.emit('user-connected', username)
-  })
+  socket.on("new-user", (room, username) => {
+    socket.join(room);
+    rooms[room].users[socket.id] = username; //Assign the users name to their socket ID and store in the object with the room name their in
+    socket.to(room).broadcast.emit("user-connected", username);
+  });
 
   //Searchs the rooms object for a room that matchs search
   //Recvies id & text from client but doesnt run line 59
-  socket.on('room-search', (id, text) => {
-    Array.from(Object.keys(rooms)).forEach((room) => {
-      console.log(room);
-      if (room.slice(0, text.length).includes(text)){
-        //socket.to(id).broadcast.emit('search-items', room)
-        console.log(room);
+  socket.on("room-search", (id, text) => {
+    Array.from(Object.keys(rooms)).forEach(room => {
+      if (room.slice(0, text.length).includes(text)) {
+        io.to(id).emit("search-items", room);
+        console.log(id, text, room);
       }
-    })
-  })
+    });
+  });
 
   //Broadcast a user disconnect message to all other users in that specific room
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     getUserRooms(socket).forEach(room => {
-      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
-      delete rooms[room].users[socket.id] // Delete users ID from the object with the room name their in
-    })
-
-  })
+      socket
+        .to(room)
+        .broadcast.emit("user-disconnected", rooms[room].users[socket.id]);
+      delete rooms[room].users[socket.id]; // Delete users ID from the object with the room name their in
+    });
+  });
 
   //Broadcast chat message to all other users in that specific room
-  socket.on('send-chat-message', (room, message) => {
-    socket.to(room).broadcast.emit('recive-chat-message', {name:  rooms[room].users[socket.id], message: message})
-  })
-})
+  socket.on("send-chat-message", (room, message) => {
+    socket.to(room).broadcast.emit("recive-chat-message", {
+      name: rooms[room].users[socket.id],
+      message: message
+    });
+  });
+});
